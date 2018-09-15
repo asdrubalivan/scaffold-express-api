e5a13617ccd1eebda95d4643f70c305b083570f4
a-suarez-vs <asdrubal.suarez@vascarsolutions.com>
2018-09-14 20:26:35 -0400

    Added generation of controller

--- src/index.js
+++ src/index.js
@@ -4,19 +4,25 @@ const {readFileSync, writeFileSync} = require('fs')
 const {resolve} = require('path')
 
 const getModelNames = modelName => {
+  const lcModel = modelName.toLowerCase()
   const pluralizedModel = p(modelName)
   const pluralizedModelLC = pluralizedModel.toLowerCase()
   return {
     modelName,
     pluralizedModel,
-    pluralizedModelLC
+    pluralizedModelLC,
+    lcModel
   }
 }
 
-const generateRoute = modelName => {
-  const routesTpl = readFileSync(resolve(__dirname, './', 'templates', 'routes.mustache')).toString()
+const generateBase = file => modelName => {
+  const routesTpl = readFileSync(resolve(__dirname, './', 'templates', `${file}.mustache`)).toString()
   const rendered = m.render(routesTpl, getModelNames(modelName))
-  return writeFileSync('routes.js', rendered)
+  return writeFileSync(`${file}.js`, rendered)
 }
 
+const generateRoute = generateBase('routes')
+const generateController = generateBase('controllers')
+
 generateRoute('User')
+generateController('User')
