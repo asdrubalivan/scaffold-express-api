59065d81227c68de2ec454171f6d06c898ec8889
a-suarez-vs <asdrubal.suarez@vascarsolutions.com>
2018-09-14 20:38:26 -0400

    Refactore generateRoute and generateController to generator.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,4 @@
-const p = require('pluralize')
-const m = require('mustache')
-const {readFileSync, writeFileSync} = require('fs')
-const {resolve} = require('path')
-
-const getModelNames = modelName => {
-  const lcModel = modelName.toLowerCase()
-  const pluralizedModel = p(modelName)
-  const pluralizedModelLC = pluralizedModel.toLowerCase()
-  return {
-    modelName,
-    pluralizedModel,
-    pluralizedModelLC,
-    lcModel
-  }
-}
-
-const generateBase = file => modelName => {
-  const routesTpl = readFileSync(resolve(__dirname, './', 'templates', `${file}.mustache`)).toString()
-  const rendered = m.render(routesTpl, getModelNames(modelName))
-  return writeFileSync(`${file}.js`, rendered)
-}
-
-const generateRoute = generateBase('routes')
-const generateController = generateBase('controllers')
+const {generateController, generateRoute} = require('./generator')
 
 generateRoute('User')
 generateController('User')
