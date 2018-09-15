37ea0d9ea32e31af422fc239f7986548064fae6f
a-suarez-vs <asdrubal.suarez@vascarsolutions.com>
2018-09-14 20:03:05 -0400

    Added routes generator

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('Hello world')
+const p = require('pluralize')
+const m = require('mustache')
+const {readFileSync, writeFileSync} = require('fs')
+const {resolve} = require('path')
+
+const getModelNames = modelName => {
+  const pluralizedModel = p(modelName)
+  const pluralizedModelLC = pluralizedModel.toLowerCase()
+  return {
+    modelName,
+    pluralizedModel,
+    pluralizedModelLC
+  }
+}
+
+const generateRoute = modelName => {
+  const routesTpl = readFileSync(resolve(__dirname, './', 'templates', 'routes.mustache')).toString()
+  const rendered = m.render(routesTpl, getModelNames(modelName))
+  return writeFileSync('routes.js', rendered)
+}
+
+generateRoute('User')
